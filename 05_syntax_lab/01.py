989a4f80d4ebceaa1e1f282e4e2a96789c69a1aa
mashaGC <mashagarmiza@gmail.com>
2016-07-04 18:17:24 +0300

    Solution for: sol-python-verint.syntax-lab/01.py

--- 05_syntax_lab/01.py
+++ 05_syntax_lab/01.py
@@ -1,5 +1,10 @@
-"""
-Write a program that reads 10 numbers from
-the user and prints the largest one
-"""
+_author_ = 'mariag'
 
+for i in range(10):
+    print 'Please insert a number. Count ', i+1
+    newInput = float(raw_input())
+    if i==0:
+        largest = newInput
+    if largest < newInput:
+        largest = newInput
+print 'largest is: ', largest
