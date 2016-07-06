722e5061f1eac5db9313d92df7c00b0ea0bacc0b
mashaGC <mashagarmiza@gmail.com>
2016-07-06 15:58:22 +0300

    Solution for: sol-python-verint.modules-lab/03.py

--- 06_modules_lab/03.py
+++ 06_modules_lab/03.py
@@ -1,11 +1,26 @@
-""" Write a program that searches current working directory
-for files larger than 1MB. Every time you find such a file print
-its name to the user.
+_author_ = 'mariag'
 
-- When the program finds a large file. It should ask the user
-  a message asking if she wants to delete it, and delete the
-  file if requested
+from sys import argv
+import os
 
-- Take threshold and path as command line arguments
-"""
+if len(argv)!= 3:
+    print 'usage: <03.py> <dir path> <size[MB]>'
+    exit(1)
+
+dirPath = argv[1]
+sizeLimit = int(argv[2])
+
+for root, dirs, files in os.walk(dirPath):
+    for name in files:
+        size = os.stat(name).st_size >> 20
+        if  size > sizeLimit:
+            print 'The file {} size is {}MB'.format(name, size)
+            answer = raw_input('Would you like to delete it? (Y/N): ')
+            if (answer.upper() == 'Y'):
+                os.remove(name)
+                print 'File {} was deleted'.format(name)
+            elif (answer.upper() == 'N'):
+                print 'File {} wasn\'t deleted'.format(name)
+
+print 'No more large files'
 
