9798ce0a07d14931665102b16a921027a1ffc507
mashaGC <mashagarmiza@gmail.com>
2016-07-13 15:21:15 +0300

    Solution for: sol-python-verint.files-lab/01.py

--- 08_files_lab/01.py
+++ 08_files_lab/01.py
@@ -3,14 +3,14 @@ _author_ = 'mariag'
 from sys import argv
 
 if len(argv)!= 3:
-    print 'usage: <01.py> <file_name1> <file_name2>'
+    print 'usage: <01.py> <file_from> <file_to>'
     exit(1)
 
-file_name1 = argv[1]
-file_name2 = argv[2]
+file_from = argv[1]
+file_to = argv[2]
 
-with open(file_name1, 'r') as f_from:
-    with open(file_name2, 'a') as f_to:
+with open(file_from, 'r') as f_from:
+    with open(file_to, 'a') as f_to:
         f_to.write('\n')
         for line in f_from:
             f_to.write(line)
