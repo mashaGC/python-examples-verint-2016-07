68d3e197fb4ee65bf77326293238864a4e11a4f4
mashaGC <mashagarmiza@gmail.com>
2016-07-15 13:23:28 +0300

    Solution for: sol-python-verint.functions-lab/03.py

--- 14_functions_lab/03.py
+++ 14_functions_lab/03.py
@@ -1,5 +1,13 @@
-"""
-Write a function that calculates the sum
-of the 10th digit from all arguments passed to it
-"""
+_author_ = 'mariag'
 
+def sum_dec(*args):
+    res = 0
+    for num in args:
+        if type(num) != int:
+            raise Exception ("sum_dec expects integer")
+        num_str = str(num)
+        if len(num_str)>1:
+            res += int(num_str[-2])
+    return res
+
+print sum_dec(1233,4,23,145) #expect 9
