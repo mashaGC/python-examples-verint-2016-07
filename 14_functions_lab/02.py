0bcd5d6a10f3fd8bcd62b92fa987cc3120455dea
mashaGC <mashagarmiza@gmail.com>
2016-07-15 13:09:37 +0300

    Solution for: sol-python-verint.functions-lab/02.py

--- 14_functions_lab/02.py
+++ 14_functions_lab/02.py
@@ -1,7 +1,9 @@
-"""
-Write a function that takes two arguments: 
-    A string
-    And a number
-If wrong types were passed in, raise an exception
-"""
+_author_ = 'mariag'
 
+def int_str(int_arg, str_arg):
+    if type(int_arg) != int:
+        raise Exception ("int_arg expects integer")
+    if type(str_arg) != str:
+        raise Exception ("str_arg expects string")
+
+int_str('3',4)
