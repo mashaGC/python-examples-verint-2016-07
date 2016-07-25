e62ebdbc181ffe9416f885b5c83d350dbf62cac3
mashaGC <mashagarmiza@gmail.com>
2016-07-25 09:06:01 +0300

    Solution for: sol-python-verint.regexp-lab/01.py

--- 18_regexp_lab/01.py
+++ 18_regexp_lab/01.py
@@ -13,13 +13,13 @@ kvDic = {}
 #load key|value to dictionary
 with open(file_key_value, 'r') as f_kv:
     for line in f_kv:
-        m = re.search('^(\w+)\s*=\s*(\w+)' , line)
-        if m is not None:
+        m = re.search('(^\w+)\s*=\s*([\w\s]+)' , line)
+        if m:
             key = m.group(1)
             value = m.group(2)
             kvDic[key] = value
 
-if kvDic.has_key(input_key):
+if input_key in kvDic:
     print input_key," = ",
     print kvDic.get(input_key)
 else:
