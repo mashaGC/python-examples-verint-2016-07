87433a7c3ee190ac4e7e4c7c894713a359993709
mashaGC <mashagarmiza@gmail.com>
2016-07-30 22:37:02 +0300

    Solution for: sol-python-verint.exceptions-lab/03.py

--- 25_exceptions_lab/03.py
+++ 25_exceptions_lab/03.py
@@ -1,4 +1,30 @@
-"""
-Write code to make the following unit test pass
-"""
+_author_ = 'mariag'
 
+import unittest
+import re
+
+class InvalidImageExt(Exception):
+    def __init__(self, msg):
+        super(InvalidImageExt, self).__init__(msg)
+
+class ImageFile(object):
+    _supported = ["jpeg","gif","bmp","png"]
+    
+    def __init__(self, file):
+        res = re.search("(^[\w]+)\.([\w]+)$",file)
+        formatIm = res.group(2)
+        if formatIm not in ImageFile._supported:
+            raise InvalidImageExt("Image format {} is not supported".format(formatIm))
+
+class TestImageFile(unittest.TestCase):
+    def test_good_ext(self):
+        try:
+            img = ImageFile("file.png")
+        except InvalidImageExt:
+            self.fail("png should be a valid file extension")
+
+    def test_bad_ext(self):
+        with self.assertRaises(InvalidImageExt):
+            img = ImageFile("file.mp3")
+
+unittest.main()
