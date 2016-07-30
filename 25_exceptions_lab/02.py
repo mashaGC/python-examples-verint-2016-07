404b3100d85576a75ae118f5be706c9090811d1a
mashaGC <mashagarmiza@gmail.com>
2016-07-30 21:41:17 +0300

    Solution for: sol-python-verint.exceptions-lab/02.py

--- 25_exceptions_lab/02.py
+++ 25_exceptions_lab/02.py
@@ -1,7 +1,14 @@
-"""
-Write a program that takes a file name
-and prints line count for the file
+_author_ = 'mariag'
 
-Alert the user politely if there was any problem opening the file
-"""
+import sys
+import os
 
+file = sys.argv[1]
+
+print file
+
+try:
+    lines = sum(1 for line in open(file))
+    print "Amount of lines in {} is {}".format(file, lines)
+except IOError as e:
+    print "Sorry, file {} not found".format(file)
